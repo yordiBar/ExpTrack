e4768e901d6e26a5e5443da55c68eb4929f31a47
Bart <bart.rybak@hotmail.com>
2021-05-30 00:53:52 +0100

    added ItemController and Index View displaying items from the DB

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,27 @@
+﻿using ExpTrack.Data;
+using ExpTrack.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpTrack.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        public ItemController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+    }
+}
