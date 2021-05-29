61b037b78f3ac5fe91e8bec5bb2b35944e18053c
Bart <bart.rybak@hotmail.com>
2021-05-30 00:30:49 +0100

    Add project files.

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using ExpTrack.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpTrack.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
